287d8b0f283aef8542125bf1e020ac64df1c571b
Jashvant Sewmangal <jashy@hotmail.nl>
2023-12-06 16:42:20 +0100

    Initial Commit

---
+++ Naruto Pagination API/Naruto_Pagination_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Naruto_Pagination_APIApp.swift
+//  Naruto Pagination API
+//
+//  Created by Jashvant Sewmangal on 06/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct Naruto_Pagination_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
